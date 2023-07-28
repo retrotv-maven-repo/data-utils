2e795d3b80abf6192eebcd2aba786a709409d07d
yjj8353 <yjj8353@gmail.com>
2023-07-28 17:02:40 +0900

    build.gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,37 @@
 plugins {
+    java
     kotlin("jvm") version "1.9.0"
+    `maven-publish`
 }
-
-group = "dev.retrotv"
-version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     testImplementation(kotlin("test"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.retrotv"
+            artifactId = "data-utils"
+            version = "0.0.1-alpha"
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
