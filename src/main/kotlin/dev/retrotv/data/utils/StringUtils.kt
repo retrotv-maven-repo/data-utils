728459a9371931eb65296371be56116cd29568d1
yjj8353 <yjj8353@gmail.com>
2023-08-06 15:16:58 +0900

    isPhone, isCellPhone, isHomePhone 메소드 추가

--- src/main/kotlin/dev/retrotv/data/utils/StringUtils.kt
+++ src/main/kotlin/dev/retrotv/data/utils/StringUtils.kt
@@ -174,3 +174,77 @@ fun intToString(value: Int, format: String): String {
     val df = DecimalFormat(format)
     return df.format(value)
 }
+
+/**
+ * <pre>
+ * 이메일 형식인지 확인합니다.
+ * </pre>
+ *
+ * @param value 확인할 값
+ * @return 이메일 형식 여부
+ */
+fun isEmail(value: String): Boolean {
+    val re = Regex("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}\$")
+    return re.matches(value)
+}
+
+/**
+ * <pre>
+ * 유선 전화번호 형식인지 확인합니다.
+ * </pre>
+ *
+ * @param value 확인할 값
+ * @return 유선 전화번호 형식 여부
+ */
+fun isHomePhone(value: String): Boolean {
+
+    // 서울
+    return if (value.substring(0 until 2) == "02") {
+        val cuttingNum = value.substring(2)
+        val re = Regex("^([-)])?\\d{3,4}-?\\d{4}\$")
+        re.matches(cuttingNum)
+
+    // 서울 외
+    } else {
+
+        // 지역 번호
+        val areaNum = value.substring(0 until 3)
+        var re = Regex("^0(31|32|33|41|42|43|44|51|52|53|54|55|61|62|63|64)\$")
+
+        // 허용 된 지역 번호
+        return if (re.matches(areaNum)) {
+            val cuttingNum = value.substring(3)
+            re = Regex("^([-)])?\\d{3,4}-?\\d{4}\$")
+            re.matches(cuttingNum)
+
+        // 그 외
+        } else { false }
+    }
+}
+
+/**
+ * <pre>
+ * 휴대 전화번호 형식인지 확인합니다.
+ * </pre>
+ *
+ * @param value 확인할 값
+ * @return 휴대 전화번호 형식 여부
+ */
+fun isCellPhone(value: String): Boolean {
+    val re = Regex("^01([016789])([-)])?\\d{3,4}-?\\d{4}\$")
+    return re.matches(value)
+}
+
+/**
+ * <pre>
+ * 유선 전화번호 형식 혹은 휴대 전화번호 형식인지 확인합니다.
+ * </pre>
+ *
+ * @param value 확인할 값
+ * @return 유선/휴대 전화번호 형식 여부
+ */
+fun isPhone(value: String): Boolean {
+    val result1 = isHomePhone(value)
+    val result2 = isCellPhone(value)
+    return result1 || result2;
+}
