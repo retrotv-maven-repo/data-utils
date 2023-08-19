a5ec55dd708edae5a77babc202bd5742e2335e79
yjj8353 <yjj8353@gmail.com>
2023-08-19 12:52:49 +0900

    주석 추가

--- src/main/kotlin/dev/retrotv/data/utils/StringUtils.kt
+++ src/main/kotlin/dev/retrotv/data/utils/StringUtils.kt
@@ -175,31 +175,79 @@ fun intToString(value: Int, format: String): String {
     return df.format(value)
 }
 
+/**
+ * <pre>
+ * 영문자 포함 여부를 반환합니다.
+ * </pre>
+ *
+ * @param value 확인할 문자열
+ * @return 영문자 포함 여부
+ */
 fun isIncludeEnglish(value: String): Boolean {
     val re = Regex(".*[a-zA-Z]+.*")
     return value.matches(re)
 }
 
+/**
+ * <pre>
+ * 영소문자 포함 여부를 반환합니다.
+ * </pre>
+ *
+ * @param value 확인할 문자열
+ * @return 영소문자 포함 여부
+ */
 fun isIncludeLowerCase(value: String): Boolean {
     val re = Regex(".*[a-z]+.*")
     return value.matches(re)
 }
 
+/**
+ * <pre>
+ * 영대문자 포함 여부를 반환합니다.
+ * </pre>
+ *
+ * @param value 확인할 문자열
+ * @return 영대문자 포함 여부
+ */
 fun isIncludeUpperCase(value: String): Boolean {
     val re = Regex(".*[A-Z]+.*")
     return value.matches(re)
 }
 
+/**
+ * <pre>
+ * 숫자 포함 여부를 반환합니다.
+ * </pre>
+ *
+ * @param value 확인할 문자열
+ * @return 숫자 포함 여부
+ */
 fun isIncludeNumber(value: String): Boolean {
     val re = Regex(".*[0-9]+.*")
     return value.matches(re)
 }
 
+/**
+ * <pre>
+ * 특수문자 포함 여부를 반환합니다.
+ * </pre>
+ *
+ * @param value 확인할 문자열
+ * @return 특수문자 포함 여부
+ */
 fun isIncludeSpecialCharacter(value: String): Boolean {
     val re = Regex(".*[^a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣\\s]+.*")
     return value.matches(re)
 }
 
+/**
+ * <pre>
+ * 한글 포함 여부를 반환합니다.
+ * </pre>
+ *
+ * @param value 확인할 문자열
+ * @return 한글 포함 여부
+ */
 fun isIncludeKorean(value: String): Boolean {
     val re = Regex(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")
     return value.matches(re)
@@ -210,7 +258,7 @@ fun isIncludeKorean(value: String): Boolean {
  * 이메일 형식인지 확인합니다.
  * </pre>
  *
- * @param value 확인할 값
+ * @param value 확인할 문자열
  * @return 이메일 형식 여부
  */
 fun isEmail(value: String): Boolean {
@@ -223,7 +271,7 @@ fun isEmail(value: String): Boolean {
  * 유선 전화번호 형식인지 확인합니다.
  * </pre>
  *
- * @param value 확인할 값
+ * @param value 확인할 문자열
  * @return 유선 전화번호 형식 여부
  */
 fun isHomePhoneNumber(value: String): Boolean {
@@ -257,7 +305,7 @@ fun isHomePhoneNumber(value: String): Boolean {
  * 휴대 전화번호 형식인지 확인합니다.
  * </pre>
  *
- * @param value 확인할 값
+ * @param value 확인할 문자열
  * @return 휴대 전화번호 형식 여부
  */
 fun isCellPhoneNumber(value: String): Boolean {
@@ -270,7 +318,7 @@ fun isCellPhoneNumber(value: String): Boolean {
  * 유선 전화번호 형식 혹은 휴대 전화번호 형식인지 확인합니다.
  * </pre>
  *
- * @param value 확인할 값
+ * @param value 확인할 문자열
  * @return 유선/휴대 전화번호 형식 여부
  */
 fun isPhoneNumber(value: String): Boolean {
