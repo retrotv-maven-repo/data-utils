550f540f309709ea49d487e4be54b7b4a091a13b
yjj8353 <yjj8353@gmail.com>
2023-10-20 21:36:23 +0900

    lcm, gcm 매개변수 Int용 메소드 추가

--- src/main/kotlin/dev/retrotv/data/utils/NumberUtils.kt
+++ src/main/kotlin/dev/retrotv/data/utils/NumberUtils.kt
@@ -4,12 +4,32 @@ package dev.retrotv.data.utils
 import java.util.LinkedList
 import java.util.Queue
 
+val ARGUMENTS_IS_BIGGER_THEN_TWO = "매개변수의 개수는 2보다 작을 수 없습니다."
+
 fun leastCommonMultiple(vararg values: Long): Long {
-    require(values.size >= 2) { "매개변수의 개수는 2보다 작을 수 없습니다." }
+    require(values.size >= 2) { ARGUMENTS_IS_BIGGER_THEN_TWO }
     val queue: Queue<Long> = LinkedList(values.toList())
 
     var lcmValue: Long? = null
-    while(queue.peek() != null) {
+    while (queue.peek() != null) {
+        val val1 = lcmValue ?: queue.poll()
+        val val2 = queue.poll()
+        lcmValue = val1?.let { lcm(it, val2) }
+    }
+
+    return lcmValue!!
+}
+
+fun leastCommonMultiple(vararg values: Int): Long {
+    require(values.size >= 2) { ARGUMENTS_IS_BIGGER_THEN_TWO }
+
+    val newValues = longArrayOf()
+    values.forEachIndexed { index, i -> newValues[index] = i.toLong() }
+
+    val queue: Queue<Long> = LinkedList(newValues.toList())
+
+    var lcmValue: Long? = null
+    while (queue.peek() != null) {
         val val1 = lcmValue ?: queue.poll()
         val val2 = queue.poll()
         lcmValue = val1?.let { lcm(it, val2) }
@@ -19,13 +39,14 @@ fun leastCommonMultiple(vararg values: Long): Long {
 }
 
 private fun lcm(value1: Long, value2: Long): Long = (value1 * value2) / gcm(value1, value2)
+private fun lcm(value1: Int, value2: Int): Long = (value1 * value2) / gcm(value1, value2)
 
 fun greatestCommonDivisor(vararg values: Long): Long {
-    require(values.size >= 2) { "매개변수의 개수는 2보다 작을 수 없습니다." }
+    require(values.size >= 2) { ARGUMENTS_IS_BIGGER_THEN_TWO }
     val queue: Queue<Long> = LinkedList(values.toList())
 
     var gcmValue: Long? = null
-    while(queue.peek() != null) {
+    while (queue.peek() != null) {
         val val1 = gcmValue ?: queue.poll()
         val val2 = queue.poll()
         gcmValue = val1?.let { gcm(it, val2) }
@@ -34,4 +55,34 @@ fun greatestCommonDivisor(vararg values: Long): Long {
     return gcmValue!!
 }
 
-private fun gcm(value1: Long, value2: Long): Long = if(value2 != 0.toLong()) gcm(value2, value1 % value2) else value1
+fun greatestCommonDivisor(vararg values: Int): Long {
+    require(values.size >= 2) { ARGUMENTS_IS_BIGGER_THEN_TWO }
+
+    val newValues = longArrayOf()
+    values.forEachIndexed { index, i -> newValues[index] = i.toLong() }
+
+    val queue: Queue<Long> = LinkedList(newValues.toList())
+
+    var gcmValue: Long? = null
+    while (queue.peek() != null) {
+        val val1 = gcmValue ?: queue.poll()
+        val val2 = queue.poll()
+        gcmValue = val1?.let { gcm(it, val2) }
+    }
+
+    return gcmValue!!
+}
+
+private fun gcm(value1: Long, value2: Long): Long =
+    if (value2 != 0.toLong()) {
+        gcm(value2, value1 % value2)
+    } else {
+        value1
+    }
+
+private fun gcm(value1: Int, value2: Int): Long =
+    if (value2 != 0) {
+        gcm(value2, value1 % value2)
+    } else {
+        value1.toLong()
+    }
