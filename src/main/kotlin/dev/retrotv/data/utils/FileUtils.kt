297577932e333a606a83f62c8cecfdbcefda0184
yjj8353 <yjj8353@gmail.com>
2024-11-11 12:52:51 +0900

    버전 업 및 Exception 내용 수정

--- src/main/kotlin/dev/retrotv/data/utils/FileUtils.kt
+++ src/main/kotlin/dev/retrotv/data/utils/FileUtils.kt
@@ -4,6 +4,7 @@ import java.io.DataInputStream
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
+import java.nio.file.InvalidPathException
 
 object FileUtils {
 
@@ -12,6 +13,9 @@ object FileUtils {
      *
      * @param file 읽어들일 파일
      * @return ByteArray(byte[])형으로 변환된 데이터
+     * @throws IOException 파일을 읽을 수 없으면 던져짐
+     * @throws InvalidPathException 파일 인스턴스로부터 Path 객체를 생성할 수 없으면 던져짐
+     * @throws SecurityException 파일 및 디렉터리 접근 권한이 없으면 던져짐
      */
     @JvmStatic
     @Throws(IOException::class)
@@ -23,8 +27,8 @@ object FileUtils {
                 fileData = ByteArray(file.length().toInt())
                 dis.readFully(fileData)
             }
-        } catch (e: IOException) {
-            throw IOException("파일을 읽어 들이는 과정에서 오류가 발생했습니다.")
+        } catch (ex: IOException) {
+            throw IOException("파일을 읽어 들이는 과정에서 오류가 발생했습니다.", ex)
         }
 
         return fileData
@@ -44,6 +48,7 @@ object FileUtils {
      *
      * @param file null 여부를 확인할 File 객체
      * @return null 혹은 0byte 여부
+     * @throws SecurityException 파일 및 디렉터리 접근 권한이 없으면 던져짐
      */
     @JvmStatic
     fun isEmpty(file: File?): Boolean = isNull(file) || (file?.length() == 0L)
